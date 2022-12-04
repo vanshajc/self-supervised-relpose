428a0c5fca621a7393cca02f2fae349fb6f74fb2
Anirudh S Chakravarthy <anirudh.s.chakravarthy@gmail.com>
2022-12-04 13:21:26 -0500

    Update tensorboard logging

--- experiments/train_relpose.sh
+++ experiments/train_relpose.sh
@@ -1,6 +1,7 @@
 # Our standard RelPose model
-python ../train.py --model_name mono+relpose \
+CUDA_VISIBLE_DEVICES=7 python ../train.py --model_name mono+relpose \
     --pose_model_type relpose \
     --models_to_load encoder depth \
     --load_weights_folder ../../checkpoints \
-    --data_path ../../data/kitti_data
+    --data_path ../../data/kitti_data \
+    --log_dir ../logs/
